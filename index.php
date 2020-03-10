00f73633a58105ad3492d8a3e33f556fa01469e0
dana harum <dana.putri96@gmail.com>
2020-03-10 19:36:14 +0700

    final

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ class Department{
 
 class Employee{
 	private $name;
-	private $title;
+	private $title = "S.Kom";
 	private $salary;
 
 
@@ -37,18 +37,21 @@ class Employee{
 		$this->name = $name;
 	}
 
+	public function getEmployeename(){
+		return $this->name;
+
+	}
+
 	public function getEmployeeTitle(){
+		return "S.Kom";
 
 	}
 
 	public function getEmployeeProfile(){
+		return "work as a marketing staff";
 
 	}
 
-	public function getEmployeeMonthlySalary($day){
-		
-
-	}
 }
 
 $company = new company();
@@ -59,10 +62,13 @@ $department = new Department();
 $department->setDepartmentName("marketing department");
 $departmentName = $department->getDepartmentName();
 
+$employee = new Employee();
+$employee->setEmployeename("Dana Putri Harum");
+$employeeName = $employee->getEmployeename();
+$employeeTitle = $employee->getEmployeeTitle();
+$employeeProfile = $employee->getEmployeeProfile();
 
-echo $companyName;
-echo "<br>";
-echo $departmentName;
+echo $companyName."<br>".$departmentName."<br>".$employeeName."<br>".$employeeTitle."<br>".$employeeProfile;
 
 
  ?>
